25e09ee6f37939a55ea4d6cd89e2fca430959685
avilca <avilca85@gmail.com>
2024-01-30 17:03:21 -0500

    creacion de usuario,Login, logout

--- tasks/views.py
+++ tasks/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
-from django.contrib.auth import login
+from django.contrib.auth import login, logout, authenticate
+from django.db import IntegrityError
 
 
 # Create your views here.
@@ -9,6 +10,7 @@ from django.contrib.auth import login
 
 def home(request):
     return render(request, 'home.html')
+
 
 def signup(request):
     if request.method == 'GET':
@@ -19,18 +21,45 @@ def signup(request):
         if request.POST['password1'] == request.POST['password2']:
             try:
                 user = User.objects.create_user(
-                    username=request.POST['username'], password=request.POST['passsword1'])
+                    username=request.POST['username'], password=request.POST['password1'])
                 user.save()
-                return redirect('tasks')               
-            except:
+                login(request, user)
+                return redirect('tasks')
+            except IntegrityError:
                 return render(request, 'signup.html', {
                     'form': UserCreationForm,
                     "error": 'Usuario ya existe'
                 })
+
         return render(request, 'signup.html', {
-            "form": UserCreationForm,
+            'form': UserCreationForm,
             "error": 'Password no coinciden'
         })
-    
+
+
 def tasks(request):
     return render(request, 'tasks.html')
+
+
+def signout(request):
+    logout(request)
+    return redirect('home')
+
+
+def signin(request):
+
+    if request.method == 'GET':
+        return render(request, 'signin.html', {
+            'form': AuthenticationForm
+        })
+    else:
+        user = authenticate(
+            request, username=request.POST['username'], password=request.POST['password'])
+        if user is None:
+            return render(request, 'signin.html', {
+                'form': AuthenticationForm,
+                'error': 'Usuario o contraseña es incorrecto'
+        })
+        else:
+            login(request, user)
+            return redirect('tasks')
